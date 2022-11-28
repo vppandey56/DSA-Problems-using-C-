89c77f3c2fca848ff96e67bd6a3507413b6f2c21
Ved Prakash Pandey <75631832+vppandey56@users.noreply.github.com>
2022-11-28 12:54:30 +0530

    Create 1st_(selection sort_problem).cpp

---
+++ Sorting"/Selection sort"/1st_(selection sort_problem).cpp
@@ -0,0 +1,38 @@
+/*
+Sample Input 1:
+1
+5
+6 2 8 4 10
+Sample Output 1:
+2 4 6 8 10
+Explanation :
+In the first step, the minimum element is 2. Thus it is swapped with the starting element of the unsorted region.
+In the second step, 4 is the minimum element. Now, it is swapped with the starting element of the unsorted region.
+Similarly, in the third step, the minimum element is 6, which is swapped accordingly.
+In the final step, all elements are arranged as per the non-decreasing order. Thus the array is sorted. 
+Sample Input 2:
+2
+2
+1 2
+4
+4 3 2 1
+Sample Output 2:
+1 2
+1 2 3 4
+
+*/
+
+#include <bits/stdc++.h> 
+void selectionSort(vector<int>& arr, int n)
+{   
+    // Write your code here.
+    for(int i=0;i<n-1;i++){
+        int minindex=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[minindex]){
+                minindex=j;
+            }
+        }
+        swap(arr[minindex],arr[i]);
+    }
+}
