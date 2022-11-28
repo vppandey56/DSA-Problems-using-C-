ef638a95d701bd31e5a2fed28fcaf2685ea73303
Ved Prakash Pandey <75631832+vppandey56@users.noreply.github.com>
2022-11-28 13:54:20 +0530

    Update !st.cpp

--- Sorting"/Bubble sort"/!st.cpp
+++ Sorting"/Bubble sort"/!st.cpp
@@ -1,1 +1,52 @@
+/*
+Sample Input 1:
+1
+5
+6 2 8 4 10
+Sample Output 1:
+2 4 6 8 10
+Sample Input 2:
+2
+2
+1 2
+4
+4 3 2 1
+Sample Output 2:
+1 2
+1 2 3 4
+*/
 
+//code
+#include <bits/stdc++.h> 
+void bubbleSort(vector<int>& arr, int n)
+{   
+    // Write your code here.
+    for(int i=1;i<n;i++){
+        
+        for(int j=0;j<n-i;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+
+//optimized
+
+#include <bits/stdc++.h> 
+void bubbleSort(vector<int>& arr, int n)
+{   
+    // Write your code here.
+    for(int i=1;i<n;i++){
+        bool swapped=false;
+        for(int j=0;j<n-i;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                swapped=true;
+            }
+        }
+        if(swapped==false){
+            break;
+        }
+    }
+}
