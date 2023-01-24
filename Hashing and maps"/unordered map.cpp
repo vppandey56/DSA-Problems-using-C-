832c817a7abb09716af47d273f9961fdaa6e712b
Ved Prakash Pandey <75631832+vppandey56@users.noreply.github.com>
2023-01-24 12:30:28 +0530

    Create unordered map.cpp

---
+++ Hashing and maps"/unordered map.cpp
@@ -0,0 +1,37 @@
+
+#include <iostream>
+#include<bits/stdc++.h> 
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        
+        cin>>arr[i];
+    }
+    //pre-compute
+    map<int,int>mpp;
+    for(int i=0;i<n;i++){
+        mpp[arr[i]]++;
+    }
+    // map stored all the value in sorted order.
+    // iterate in the map
+    // for(auto it:mpp){
+    //     cout<<it.first<<"->"<<it.second<<endl;
+    // }
+    
+    int q;
+    cin>>q;
+    while(q--){
+        int number;
+        cin>>number;
+        //fetch
+        cout<<mpp[number]<<endl; 
+    }
+
+    return 0;
+}
+//time complexity-O(1) in best and avg case and O(N) in worst case beacuse of internal collision
